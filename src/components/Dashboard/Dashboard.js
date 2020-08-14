cf66d250eb0b08febe0a758cf3b0c1b4c1e2adce
karenChernandez <karenChernandez@live.com>
2020-08-14 15:46:28 -0700

    stillworking on it

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {getPost} from '../../redux/postAisle';
 
 
 
@@ -9,10 +10,11 @@ function Dashboard(){
     return (<div>
         <h1>This is the Dashboard Component</h1>
         
+        
      
     </div>
     )
 }
 
 const mapStateToProps = state => state;
-export default connect(mapStateToProps)  (Dashboard);
+export default connect(mapStateToProps, {getPost})  (Dashboard);
